3ee43aeeca0fe77be1bac9e42b5a143ce8030f7d
Christopher O'Hara <christopher_ohara@hotmail.com>
2014-10-22 12:26:27 -0400

    Adding the iOS project

---
+++ Code/Bonzai-iOS/Bonzai-iOSTests/Bonzai_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Bonzai_iOSTests.swift
+//  Bonzai-iOSTests
+//
+//  Created by CSSE Department on 10/22/14.
+//  Copyright (c) 2014 Rose-Hulman CSSE Department. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Bonzai_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
