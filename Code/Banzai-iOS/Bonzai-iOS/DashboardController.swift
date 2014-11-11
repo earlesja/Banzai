7113a4dc11c9d16c44c37c199edd3664efccdd5b
Christopher O'Hara <christopher_ohara@hotmail.com>
2014-11-11 14:52:36 -0500

    Fixing some design issues, added more layouts

---
+++ Code/Banzai-iOS/Bonzai-iOS/DashboardController.swift
@@ -0,0 +1,15 @@
+//
+//  DashboardController.swift
+//  Banzai-iOS
+//
+//  Created by CSSE Department on 11/11/14.
+//  Copyright (c) 2014 Rose-Hulman CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController, SideMenuControllerDelegate {
+    
+    var delegate = DashboardViewControllerDelegate?
+    
+}
